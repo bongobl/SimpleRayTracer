cf36295cef66264cb38dfeea47916c170bbdb58c
Alex Lui <alexb.lui@gmail.com>
2018-12-10 17:31:21 -0800

    Started using tiny_obj_loader

--- src/Model.cpp
+++ src/Model.cpp
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <map>
 #include "../include/Model.h"
 
 #define TINYOBJLOADER_IMPLEMENTATION
@@ -12,67 +13,69 @@ Model::Model(string model_filename){
 	scale = glm::vec3(1,1,1);
 	toWorld = glm::mat4(1.0f);
 	toWorld3x3 = glm::mat3(1.0f);
-
-	//Load Mesh
-	FILE* fp;
-	char currLine[BUFSIZ];
-	float x, y, z;
-	unsigned int v1, v2, v3, n1, n2, n3, t1, t2, t3;
-
-	fopen_s(&fp, model_filename.c_str(), "rb");
-	if (fp == NULL) {
-		std::cerr << "***Error loading file " << model_filename << "***" << std::endl;
-		system("pause");
-		exit(-1);
-	}
-
-	std::cout << "Parsing " << model_filename << "..." << std::endl;
 	
+	tinyobj::attrib_t attrib;
+    std::vector<tinyobj::shape_t> shapes;
+    std::vector<tinyobj::material_t> materials;
+    std::string err;
+    if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &err, model_filename.c_str())) {
+        throw std::runtime_error("Failed to load model " + model_filename);
+    }
 
-	while (fgets(currLine, BUFSIZ, fp) != NULL) {
+    
+    std::map<Vertex,unsigned int> uniqueVertices;
+    std::vector<unsigned int> indexBuffer;
 
-		//Process Vertex Attributes
-		if (currLine[0] == 'v') {
 
-			//read vertex normal (normals are read first, so only push back vector here)
-			if (currLine[1] == 'n' && currLine[2] == ' ') {
-				sscanf_s(currLine, "vn %f %f %f", &x, &y, &z);
-				vertices.push_back( new Vertex(glm::vec3(0,0,0), glm::vec2(0,0), glm::vec3(x,y,z)) );
-			}
+    for(const auto& shape : shapes){
+    	for(const auto& index : shape.mesh.indices){
+    		Vertex tentative;
 
-			//read in texCoords
-			else if (currLine[1] == 't' && currLine[2] == ' ') {
-				sscanf_s(currLine, "vt %f %f", &x, &y);
-				vertices.back()->texCoord = glm::vec2(x, y);
+    		//read in position
+    		tentative.position = glm::vec3(
+    			attrib.vertices[3 * index.vertex_index + 0],
+    			attrib.vertices[3 * index.vertex_index + 1],
+    			attrib.vertices[3 * index.vertex_index + 2]
+    		);
 
-			}
-			//read vertex position
-			else if (currLine[1] == ' ') {
-				sscanf_s(currLine, "v %f %f %f", &x, &y, &z);
-				vertices.back()->position = glm::vec3(x,y,z);
-			}	
-		}
+    		//read in normal
+    		tentative.setNormal(glm::vec3(
+    			attrib.normals[3 * index.normal_index + 0],
+    			attrib.normals[3 * index.normal_index + 1],
+    			attrib.normals[3 * index.normal_index + 2]
+    		));
 
-		//Process Triangles
-		else if (currLine[0] == 'f' && currLine[1] == ' ') {
-			sscanf_s(currLine, "f %i/%i/%i %i/%i/%i %i/%i/%i", &v1, &n1, &t1, &v2, &n2, &t2, &v3, &n3, &t3);
-			faces.push_back( Triangle(vertices[v1 - 1], vertices[v2 - 1], vertices[v3 - 1]) );
-		}
+    		//red in UV texCoords
+    		tentative.texCoord = glm::vec2(
+    			attrib.texcoords[2 * index.texcoord_index + 0],
+                attrib.texcoords[2 * index.texcoord_index + 1]
+    		);
 
-	}//END WHILE
-	
+    		//if vertex is new, add to map with index being current size of vertices array
+    		if(uniqueVertices.count(tentative) == 0){
+    			uniqueVertices[tentative] = vertices.size();
+    			indexBuffer.push_back(vertices.size());
+    			vertices.push_back(new Vertex(tentative));
 
-	//report model stats
-	cout << "Loaded Model " << model_filename << " with " << vertices.size() << " vertices and " << faces.size() << " faces" << endl;
+    		}else{
+    			indexBuffer.push_back(uniqueVertices[tentative]);
+    		}
+		
 
-	//Deep copy vertices to rawVertices
-	for (unsigned int i = 0; i < vertices.size(); ++i) {
+    	}
+    }
+
+    //use index buffer to construct all triangles
+    for(unsigned int i = 0; i < indexBuffer.size(); i += 3){
+    	Triangle currTriangle(vertices[indexBuffer[i]], vertices[indexBuffer[i + 1]], vertices[indexBuffer[i + 2]]);
+    	faces.push_back(currTriangle);
+    }
+
+    //store model space vertices for calculating toWorld of vertices during transformation
+    for (unsigned int i = 0; i < vertices.size(); ++i) {
 		rawVertices.push_back(new Vertex(*vertices.at(i)));
 	}
-
-	//close file after reading
-	fclose(fp);
-
+	cout << "Loaded model " << model_filename << endl;
 	
 } 
 
